ad3887b0e40bc443374ac0805aefdbffa9aac1dd
SINGER Jules <jules.singer@telecomnancy.eu>
2023-11-29 09:25:54 +0100

    Insert protein2ipr data in mongo db

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 from flask import Flask
 from flask_pymongo import PyMongo
-
+from script_protein2ipr import process_protein2ipr
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/proteins"
 mongo = PyMongo(app)
 
 @app.route('/')
 def hello():
-    mongo.db.inventory.insert_one({'name': 'test'})
-    mongo.db.proteins.insert_one({'name': 'test'})
+    dictionnaire = process_protein2ipr()
+    for key in dictionnaire:
+        # insert only if not already in database
+        if mongo.db.proteins.find_one({'protein_id': key}) == None:
+            mongo.db.proteins.insert_one({'protein_id': key, 'iprs': dictionnaire[key]})
+    
     return '<h1>Hello, World!</h1>'
 
 app.run(debug=True)
---
+++ script_protein2ipr.py
@@ -0,0 +1,17 @@
+def process_protein2ipr():
+    # Création du dictionnaire
+    dictionnaire = {}
+    # read protein2ipr file
+    f = open("protein2ipr_extrait.txt", "r")
+    for line in f:
+        elements = line.split('\t')
+        identifiant = elements[0]
+        ipr = elements[1]
+        if identifiant not in dictionnaire:
+            dictionnaire[identifiant] = []
+        dictionnaire[identifiant].append(ipr)
+    
+    # Fermeture du fichier
+    f.close()
+
+    return dictionnaire
